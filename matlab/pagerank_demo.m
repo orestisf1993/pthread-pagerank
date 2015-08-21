ae9d2a2147e954348ad005a8dba81009ce814f44
orestisf <orestisf1993@gmail.com>
2015-08-21 16:16:17 +0300

    Add initial matlab code

---
+++ matlab/pagerank_demo.m
@@ -0,0 +1,27 @@
+% A simple demo of google's pagerank algorithm.
+% It uses the pagerank implementation provided 
+% by Cleve Moler and MathWorks
+%
+% author: Nikos Sismanis
+% date: Jan 2014
+
+
+% create a small data set of web pages
+[U, G] = surfer('http://www.amazon.com/', 100);
+
+
+% Eliminate any self-referential links
+G = G - diag(diag(G));
+
+
+% Use the power method to compute the eigenvector that correspond to the
+% largest eigenvalue (Ranks)
+x = pagerankpow(G);
+
+
+% plot the ranks
+figure
+bar(x)
+title('Page Ranks')
+
+
