f1911d0ef7575e38197d7558c0b9b7746bdbf4d9
Chamzas Konstantinos <kostas93@hotmail.com>
2015-09-27 12:05:10 +0300

    Changed node_read.m in order to skip comments

--- matlab/node_read.m
+++ matlab/node_read.m
@@ -2,8 +2,8 @@ function G = node_read(filename)
 
 f = fopen(filename,'r');
 
-A = fscanf(f, '%d %d\n', [2 Inf]);
-A = A';
+A = textscan(f, '%f %f','CommentStyle','#');
+A = [A{1} , A{2}];
 x = A(:,2) + 1;
 y = A(:,1) + 1;
 size = max(max(x),max(y));
